4b0d05178a0a8b844e129483fdf9d1c216170ba5
valmlk <valyushka.malko@gmail.com>
2024-03-09 23:43:02 +0100

    add loading and error

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,12 +3,15 @@ import css from './ContactList.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 // import { deleteContact } from '../../redux/contactsSlice';
 import { deleteContact } from '../../redux/operations';
-import { selectVisibleContacts } from '../../redux/selectors';
+import { selectError, selectVisibleContacts } from '../../redux/selectors';
 import { fetchContacts } from '../../redux/operations';
 import { useEffect } from 'react';
+import { selectLoading } from '../../redux/selectors';
 
 const ContactList = () => {
   const dispatch = useDispatch();
+  const loading = useSelector(selectLoading);
+  const error = useSelector(selectError);
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -16,24 +19,27 @@ const ContactList = () => {
   // const contactsState = useSelector(state => state.contacts.items);
   // const filterState = useSelector(state => state.filter && state.filter.name);
 
-
   // const visibleContacts = selectedContacts.filter(contact =>
   //   contact.name.toLowerCase().includes(filter.toLowerCase())
   // );
-  const visibleContacts = useSelector(selectVisibleContacts)
+  const visibleContacts = useSelector(selectVisibleContacts);
 
   return (
-    <ul className={css.list}>
-      {visibleContacts.map(contact => (
-        <Contact
-          key={contact.id}
-          contact={contact}
-          onDelete={id => {
-            dispatch(deleteContact(id));
-          }}
-        />
-      ))}
-    </ul>
+    <div>
+      {loading && !error && <p>Loading...</p>}
+      {error && <p>Ooops, something went wrong</p>}
+      <ul className={css.list}>
+        {visibleContacts.map(contact => (
+          <Contact
+            key={contact.id}
+            contact={contact}
+            onDelete={id => {
+              dispatch(deleteContact(id));
+            }}
+          />
+        ))}
+      </ul>
+    </div>
   );
 };
 
